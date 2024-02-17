12c9a2945f698878f14508919ab6384e4cbc342d
unknown <claponsie@gmail.com>
2024-02-17 10:07:07 -0500

    Added the project structure

---
+++ ServiceB/MainService.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceB;
+
+public class MainService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+       while (!stoppingToken.IsCancellationRequested)
+       {
+           Console.WriteLine("ServiceB is running.");
+           await Task.Delay(1000, stoppingToken);
+       } 
+    }
+}
